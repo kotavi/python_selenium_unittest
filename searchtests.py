db3f491bc8c85542c333f39b3ba73cd821c31012
Tetiana Korchak <korchak.tetiana@gmail.com>
2020-01-15 17:29:07 -0600

    Finding elements

--- searchtests.py
+++ searchtests.py
@@ -15,26 +15,27 @@ class SearchTests(unittest.TestCase):
         cls.driver.maximize_window()
         cls.driver.get("http://demo.magentocommerce.com/")
 
+        search_button = cls.driver.find_element_by_class_name('fa-search')
+        search_button.click()
+
+    def test_seach_text_field_max_length(self):
+        search_field = WebDriverWait(self.driver, 10).until(
+            expected_conditions.element_to_be_clickable((By.CLASS_NAME, "search-query")))
+        self.assertEqual('255', search_field.get_attribute('maxlength'))
+
     def test_search_by_event(self):
-        self.search_button = self.driver.find_element_by_class_name('fa-search')
-        self.search_button.click()
+        search_field = WebDriverWait(self.driver, 10).until(expected_conditions.element_to_be_clickable((By.CLASS_NAME, "search-query")))
+        search_field.send_keys("eTail West type:event")
+        search_field.submit()
 
-        self.search_field = WebDriverWait(self.driver, 10).until(expected_conditions.element_to_be_clickable((By.CLASS_NAME, "search-query")))
-        assert self.search_field.is_displayed()
-        self.search_field.send_keys("eTail West type:event")
-        self.search_field.submit()
-
-        search_results = self.driver.find_elements_by_xpath("//h3[@class='result-title']/a")
-        self.assertEqual(2, len(search_results))
+        search_result = self.driver.find_element_by_class_name("search-results.node-results")
+        list_of_results = search_result.find_elements_by_tag_name('li')
+        self.assertEqual(2, len(list_of_results))
 
     def test_search_by_blog_post(self):
-        self.search_button = self.driver.find_element_by_class_name('fa-search')
-        self.search_button.click()
-
-        self.search_field = WebDriverWait(self.driver, 10).until(expected_conditions.element_to_be_clickable((By.CLASS_NAME, "search-query")))
-        assert self.search_field.is_displayed()
-        self.search_field.send_keys("Cabinets type:blog_post")
-        self.search_field.submit()
+        search_field = WebDriverWait(self.driver, 10).until(expected_conditions.element_to_be_clickable((By.CLASS_NAME, "search-query")))
+        search_field.send_keys("Cabinets type:blog_post")
+        search_field.submit()
 
         search_results = self.driver.find_elements_by_xpath("//h3[@class='result-title']/a")
         self.assertEqual(2, len(search_results))
